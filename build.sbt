06cd2dce2085280641275c9b1bf76a8eccba91fe
yakisuzu <yakisuzu@gmail.com>
2019-04-02 16:11:20 +0900

    practice1

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "scala"
+
+version := "0.1"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-core"   % "1.6.0",
+  "org.typelevel" %% "cats-effect" % "1.2.0",
+)
---
+++ src/main/scala/practice1.scala
@@ -0,0 +1,52 @@
+
+
+object PrintMonad extends App {
+
+  import java.util.Scanner
+
+  import cats.effect.IO
+
+  case class InputData(a: Int, b: Int, c: Int, s: String)
+
+  private val res: Either[Throwable, String] = IO(new java.util.Scanner(System.in))
+    .bracket(createAnswer)(scannerClose)
+    .attempt
+    .unsafeRunSync()
+
+  res match {
+    case Right(s) => println(s)
+    case Left(e) => e.printStackTrace()
+  }
+
+  def createAnswer(sc: Scanner): IO[String] =
+    for {
+      input <- getInputData(sc)
+      ans <- createAnswerStr(input)
+    } yield ans
+
+  def getInputData(sc: Scanner): IO[InputData] = IO {
+    val a = sc.nextInt
+    val b, c = sc.nextInt
+    val s = sc.next
+    InputData(a, b, c, s)
+  }
+
+  def createAnswerStr(input: InputData): IO[String] = IO {
+    s"${input.a + input.b + input.c} ${input.s}"
+  }
+
+  def scannerClose(sc: Scanner) = IO {
+    sc.close()
+  }
+}
+
+object Main extends App {
+
+  import java.util.Scanner
+
+  val input = {
+    val s = new Scanner(System.in)
+    (s.nextInt, s.nextInt, s.nextInt, s.next)
+  }
+  println(s"${input._1 + input._2 + input._3} ${input._4}")
+}
