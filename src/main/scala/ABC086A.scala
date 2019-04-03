d8781c48bd009fcee7001197f2b1bf8ec18f6f71
yakisuzu <yakisuzu@gmail.com>
2019-04-03 14:18:30 +0900

    tuika

--- src/main/scala/ABC086A.scala
+++ src/main/scala/ABC086A.scala
@@ -8,3 +8,15 @@ object ABC086A extends App {
   // 積が奇数なら Odd と、 偶数なら Even と出力せよ。
   println(if (i._1 * i._2 % 2 == 1) "Odd" else "Even")
 }
+
+object ABC086Aex extends App {
+  val i = {
+    val s = new java.util.Scanner(System.in)
+    (s.nextInt, s.nextInt)
+  }
+
+  println((i._1 * i._2).toString.last.toInt % 2 match {
+    case 1 => "Odd"
+    case _ => "Even"
+  })
+}
